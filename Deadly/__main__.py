d35611f297559bcc7b2bfc642ce5a76cce510f99
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-24 07:02:57 +0530

    Update __main__.py

--- Deadly/__main__.py
+++ Deadly/__main__.py
@@ -1,1 +1,18 @@
+import asyncio
+from pytgcalls import idle
+from Deadly import call_py, bot
+LOG = "TheDeadlyBots"
 
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    await bot.send_message(LOG, "Started DeadlyVC Successfully") 
+    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
