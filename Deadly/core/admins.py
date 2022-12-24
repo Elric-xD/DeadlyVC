ecc7aef40f54561a5d519cdfb217ad16a29aa005
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-24 06:50:55 +0530

    Update admins.py

--- Deadly/core/admins.py
+++ Deadly/core/admins.py
@@ -1,6 +1,6 @@
 from typing import List
 from pyrogram.types import Chat
-from .Cache.admins import get as gett, set
+from Deadly.core.Cache.admins import get as gett, set
 
 async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
