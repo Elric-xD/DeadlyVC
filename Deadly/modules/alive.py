85b7c88da62e29f302127cf7a7771c3d4356ceaf
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-26 17:25:00 +0530

    Update alive.py

--- Deadly/modules/alive.py
+++ Deadly/modules/alive.py
@@ -37,11 +37,12 @@ alive_txt = f"""
 @app.on_message(filters.command("alive") & ~filters.edited)
 async def alive(client, m: Message):  
     await m.delete() 
+    chat_id = m.chat.id
     sj = await m.reply_text("𝐂𝐫𝐞𝐚𝐭𝐢𝐧𝐠 𝐀𝐥𝐢𝐯𝐞 𝐌𝐞𝐬𝐬𝐚𝐠𝐞. . .")    
     owner = "ELRIC-XD" #JUST FOR FUN
     await sj.delete() 
     msg = await app.send_photo(
-        chat_id=m.chat.id, 
+        chat_id=chat_id, 
         photo=ALIVE_PIC, 
         caption=alive_txt
     )
