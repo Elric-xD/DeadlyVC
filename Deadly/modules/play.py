5127929ea050ba85251becede74ccf4a32310984
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-29 10:34:12 +0530

    Update play.py

--- Deadly/modules/play.py
+++ Deadly/modules/play.py
@@ -255,8 +255,8 @@ async def get_play_status(client: Client, message: Message):
 
 # PARTICIPANT LIST
 
-@Client.on_message(command(["maxvc", f"maxvc@{BOT_USERNAME}"]) & other_filters)
-async def get_play_status(client: Client, message: Message):
+@Client.on_message(command(["vclist", f"vclist@{BOT_USERNAME}"]) & other_filters)
+async def get_user_status(client: Client, message: Message):
     await message.delete()
     bc = call_py.get_participants(message.chat.id)
     await message.reply_text(f"Participant List: {bc}")
