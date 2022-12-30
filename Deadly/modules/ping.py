9a24b1ca40606d820623ea4bd306a11161ccf28a
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-30 09:46:07 +0530

    Create ping.py

---
+++ Deadly/modules/ping.py
@@ -0,0 +1,40 @@
+import time
+
+import psutil
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.types import Message
+from Deadly import BOT_NAME, PING_URL
+from Deadly import bot as app, _boot_
+from strings import get_command
+from Deadly import bot as app
+from Deadly import call_py as Akki
+from Deadly.core.Cache.format import get_readable_time
+
+# load
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - _boot_)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    UP = f"{get_readable_time((bot_uptime))}"
+    CPU = f"{cpu}%"
+    RAM = f"{mem}%"
+    DISK = f"{disk}%"
+    return UP, CPU, RAM, DISK
+
+# run
+
+
+@app.on_message(filters.command("speedtest") & ~filters.edited)
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_URL,
+        caption="**Pinging. . . **"
+    )
+    start = datetime.now()
+    pytgping = await Yukki.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(f"{BOT_NAME} ɪs ᴡᴏʀᴋɪɴɢ ɢᴏᴏᴅ ᴡɪᴛʜ ᴀ ᴘɪɴɢ ᴏғ  {resp}ᴍs.\n\nʙᴏᴛ's ᴜᴘᴛɪᴍᴇ: {UP}\n\nᴛᴏᴛᴀʟ ᴏғ  {DISK} sᴇʀᴠᴇʀ's sᴛᴏʀᴀɢᴇ.\n\nᴄᴘᴜ ʟᴏᴀᴅ ɪs ᴀʙᴏᴜᴛ {CPU} \n\nᴡɪᴛʜ ᴀ ᴄᴏɴsᴜᴍᴘᴛɪᴏɴ ᴏғ {RAM} ʀᴀᴍ. \nᴘʏ-ᴛɢᴄᴀʟʟs ᴄʟɪᴇɴᴛ ʜᴀᴠɪɴɢ ᴘɪɴɢ ᴏғ {pytgping} ᴍs.")
+
