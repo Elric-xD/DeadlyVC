03fd77ecdbd61ff0167b0ddd77a5bbee8587bd96
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-31 09:43:24 +0530

    Done

--- Deadly/database/__init__.py
+++ Deadly/database/__init__.py
@@ -1,1 +1,9 @@
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+from Deadly import DB_URL
+client = MongoClient(DB_URL)
+dbd = client["Deadly"]
+db = dbd
 
+MONGODB_CLI = AsyncIOMotorClient(DB_URL)
+dbb = MONGODB_CLI.program
