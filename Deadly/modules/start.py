89d63fd6c9087b83c58185d6a8d0930d356b3a5b
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-31 23:45:37 +0530

    Update start.py

--- Deadly/modules/start.py
+++ Deadly/modules/start.py
@@ -1,19 +1,22 @@
-
+import asyncio
+from time import time
+from datetime import datetime
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
 from Deadly import (
     ASSISTANT_NAME,
     BOT_NAME,
     BOT_USERNAME,
     GROUP_SUPPORT,
     OWNER_NAME,
+    LOG_ID, 
     UPDATES_CHANNEL,
 )
-from Deadly.core.filters import other_filters2
-from time import time
-from Deadly.core.filters import command
-from datetime import datetime
+from Deadly import bot as app
+from Deadly.core.filters import other_filters2, command
 from Deadly.core.decorators import authorized_users_only
+from youtubesearchpython.__future__ import VideosSearch
 
 START_TIME = datetime.utcnow()
 START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
@@ -36,10 +39,68 @@ async def _human_time_duration(seconds):
             parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
     return ", ".join(parts)
 
+loop = asyncio.get_running_loop()
 
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-        await message.reply_text(
+
+@app.on_message(filters.command("start") & filters.private & ~filters.edited)
+async def start_comm(client, message: Message):
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+        if name[0:3] == "inf":
+            m = await message.reply_text("🔎 Fetching Info!")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[
+                    0
+                ]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+            searched_text = f"""
+🔍__**Video Track Information**__
+❇️**Title:** {title}
+⏳**Duration:** {duration} Mins
+👀**Views:** `{views}`
+⏰**Published Time:** {published}
+🎥**Channel Name:** {channel}
+📎**Channel Link:** [Visit From Here]({channellink})
+🔗**Video Link:** [Link]({link})
+⚡️ __Searched Powered By {BOT_NAME}__"""
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="🎥 Watch ", url=f"{link}"
+                        ),
+                        InlineKeyboardButton(
+                            text="🔄 Close", callback_data="cls"
+                        ),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+                message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                parse_mode="markdown",
+                reply_markup=key,
+            )
+            sender_id = message.from_user.id
+            sender_name = message.from_user.first_name
+            await app.send_message(
+                LOG_ID,
+               f"{message.from_user.mention} has just started bot to check <code>VIDEO INFORMATION</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
+                )
+    else:
+        try:
+            await message.reply_text(
         f"""Hello, {message.from_user.mention()}
 ๏ My name is {BOT_NAME}
 ๏ I'm Telegram Music Bot with only useful Featured. 
