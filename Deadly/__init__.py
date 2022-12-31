51c84c8b85020234a81dc5b657b310c840b76f11
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-12-31 10:48:29 +0530

    Update __init__.py

--- Deadly/__init__.py
+++ Deadly/__init__.py
@@ -13,19 +13,31 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 load_dotenv()
 admins = {}
+
+#REQUIRED
 SESSION_NAME = getenv("SESSION_NAME", "")
 BOT_TOKEN = getenv("BOT_TOKEN")
 API_ID = int(getenv("API_ID", "8186557"))
 API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
 OWNER_NAME = getenv("OWNER_NAME", "Elric_xD")
+DB_URL = getenv("DB_URL", "mongodb+srv://blaze09:Blaze0786@cluster0.ihdjovn.mongodb.net/?retryWrites=true&w=majority") 
+
 ALIVE_NAME = getenv("ALIVE_NAME", "DeadlyVC")
+
+#SUPPPORT LIST
 GROUP_SUPPORT = getenv("GROUP_SUPPORT")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+
+#EXTRA
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AMANTYA1/RaiChu-MusicV2")
+
+
+#IMAGES
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
