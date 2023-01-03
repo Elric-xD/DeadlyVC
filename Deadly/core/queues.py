abb79d7ce07d597c6badccb045a67a7d403b4ae4
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2023-01-03 13:04:01 +0530

    Done

--- Deadly/core/queues.py
+++ Deadly/core/queues.py
@@ -1,33 +1,43 @@
-""" Better to load int(chat_id) """
+from typing import Dict
+from asyncio import Queue, QueueEmpty as Empty
 
-QUEUE = {}
 
-def add_to_queue(chat_id, songname, link, ref, type, quality):
-   if chat_id in QUEUE:
-      chat_queue = QUEUE[chat_id]
-      chat_queue.append([songname, link, ref, type, quality])
-      return int(len(chat_queue)-1)
-   else:
-      QUEUE[chat_id] = [[songname, link, ref, type, quality]]
+queues: Dict[int, Queue] = {}
 
-def get_queue(chat_id):
-   if chat_id in QUEUE:
-      chat_queue = QUEUE[chat_id]
-      return chat_queue
-   else:
-      return 0
 
-def pop_an_item(chat_id):
-   if chat_id in QUEUE:
-      chat_queue = QUEUE[chat_id]
-      chat_queue.pop(0)
-      return 1
-   else:
-      return 0
-      
-def clear_queue(chat_id):
-   if chat_id in QUEUE:
-      QUEUE.pop(chat_id)
-      return 1
-   else:
-      return 0
+async def put(chat_id: int, **kwargs) -> int:
+    if chat_id not in queues:
+        queues[chat_id] = Queue()
+    await queues[chat_id].put({**kwargs})
+    return queues[chat_id].qsize()
+
+
+def get(chat_id: int) -> Dict[str, str]:
+    if chat_id in queues:
+        try:
+            return queues[chat_id].get_nowait()
+        except Empty:
+            return None
+
+
+def is_empty(chat_id: int) -> bool:
+    if chat_id in queues:
+        return queues[chat_id].empty()
+    return True
+
+
+def task_done(chat_id: int):
+    if chat_id in queues:
+        try:
+            queues[chat_id].task_done()
+        except ValueError:
+            pass
+
+
+def clear(chat_id: int):
+    if chat_id in queues:
+        if queues[chat_id].empty():
+            raise Empty
+        else:
+            queues[chat_id].queue = []
+    raise Empty
